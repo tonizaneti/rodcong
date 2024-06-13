3d91f9f29be948dd1e86d9af69b6f83236f0c7d7
Antonio Delvair Zaneti <antonio.zaneti@gmail.com>
2024-06-12 21:33:07 -0300

    Add files via upload

---
+++ topo.php
@@ -0,0 +1,193 @@
+	<link rel="stylesheet" type="text/css" href="./index.css" media="all" />
+	<!--[if IE]>
+	<style type="text/css" media="all">.borderitem {border-style: solid;}</style>
+	<![endif]-->
+
+
+<div id="maintopo">
+	<div id="logo">
+	<div style="vertical-align:bottom; width:200px">
+<?
+$server = $_SERVER['SERVER_NAME'];
+$endereco = $_SERVER ['REQUEST_URI'];
+//echo "http://" . $server . $endereco;
+?>
+
+<?
+   if(($server == "www.tvnarua.com.br")|| ($server == "tvnarua.com.br"))
+   {
+
+
+	echo"<a href='index.php'><img style='background-color:#CCCCCC'; src='../imagens/logo_tv.png' alt='TV Na Rua' width='101' height='202' /></a>";
+
+	?>
+
+	<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-2133048-14']);
+  _gaq.push(['_setDomainName', 'tvnarua.com.br']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+
+	
+	<?
+   }
+   
+   else if(($server == "www.corneliodigital.com")|| ($server == "corneliodigital.com"))
+   {
+	echo"<a href='index.php'><img style='background-color:#CCCCCC'; src='../imagens/logocd2.jpg' alt='TV Na Rua' width='101' height='202' /></a>";
+	
+	?>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-2133048-14']);
+  _gaq.push(['_setDomainName', 'corneliodigital.com']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+
+
+<?
+   }
+   else if(($server == "www.banddigital.com.br")|| ($server == "banddigital.com.br"))
+   {
+	echo"<a href='index.php'><img style='background-color:#CCCCCC'; src='../imagens/logo_band.png' alt='Band Digital' width='101' height='202' /></a>";
+	?>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-2133048-14']);
+  _gaq.push(['_setDomainName', 'banddigital.com.br']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>  
+
+<?
+}
+   else if(($server == "www.nossoclick.com.br")|| ($server == "nossoclick.com.br")||($server == "www.nossoclick.com")|| ($server == "nossoclick.com"))
+   {
+  echo"<a href='index.php'><img style='background-color:#CCCCCC'; src='../imagens/nossoclick2.png' alt='Nosso Click' width='100' height='100' /></a>";
+  ?>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-2133048-14']);
+  _gaq.push(['_setDomainName', 'banddigital.com.br']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>  
+
+
+<?
+}
+   else if(($server == "www.plantaonossoclick.com.br")|| ($server == "plantaonossoclick.com.br"))
+   {
+  echo"<a href='index.php'><img style='background-color:#CCCCCC'; src='../imagens/plantaoNossoClick.png' alt='Plantao Nosso Click' width='100' height='100' /></a>";
+  ?>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-2133048-14']);
+  _gaq.push(['_setDomainName', 'banddigital.com.br']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>  
+
+
+
+
+
+
+
+
+<?
+}
+   else if(($server == "www.corneliodigital.com.br")|| ($server == "corneliodigital.com.br"))
+   {
+	echo"<a href='index.php'><img style='background-color:#CCCCCC'; src='../imagens/logocd2.jpg' alt='TV Na Rua' width='101' height='202' /></a>";
+	
+	?>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-2133048-14']);
+  _gaq.push(['_setDomainName', 'corneliodigital.com.br']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+
+
+<?
+   }
+   else 
+   {
+	echo"<a href='index.php'><img style='background-color:#CCCCCC'; src='../imagens/logocd2.jpg' alt='TV Na Rua' width='101' height='202' /></a>";
+	?>
+
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-2133048-14']);
+  _gaq.push(['_setDomainName', 'corneliodigital.com.br']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+<?
+   }
+
+
+?>
+</div><br />
+<a href="https://www.facebook.com/tvnarua" target="_blank"><img src="radiodigital/facebook-logo.png" alt="Facebook" width="30" height="30" /></a>
+<a href="http://twitter.com/corneliodigital" target="_blank"><img src="radiodigital/twitter.gif" width="39" height="33" border="0" usemap="#Map" /></a>
+
+
+	</div>
+		<div id="bannersup"><? include "topoeve.php"; ?></div>
+		<div class="clearFloat"></div>
+</div>
